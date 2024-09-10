1437e6859b3a329d440686ed5a367f8ce7bdd8fc
Bruno Shizukuda <shizukudabruno@gmail.com>
2024-09-10 13:11:51 -0300

    cadastrar metas

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-const { select } = require('@inquirer/prompts') 
+const { select, input } = require('@inquirer/prompts') 
+
+let meta = {
+    value: "Tomar 3L de água por dia",
+    checked: false,
+
+}
+
+let metas = [meta]
+
+const cadastrarMeta = async() => {
+    const meta = await input({message:"Digite a meta:"})
+
+    if(meta.length == 0) {
+        console.log('A meta não pode ser vazia')
+    }
+
+    metas.push(
+        {value: meta, checked: false}
+    )
+}
+
+
 
 const start = async () => {
     while(true) {
@@ -23,7 +45,7 @@ const start = async () => {
 
         switch(opcao) {
             case "cadastrar":
-                console.log('vamos cadastrar')
+                await cadastrarMeta()
                 break
             case "Listar":
                 console.log('vamos listar')
